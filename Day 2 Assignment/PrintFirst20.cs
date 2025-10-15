8a2dbf672e6baf91d692ab147e5788dcd4144724
Adarsh S Kumar <adarsh.s@geekywolf.com>
2025-10-15 16:17:47 +0530

    third commit

---
+++ Day 2 Assignment/PrintFirst20.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class PrintFirst20
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
