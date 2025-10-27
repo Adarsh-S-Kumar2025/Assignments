11b9852da26d1ac79dd574d87f29050f6615ade3
Adarsh-S-Kumar2025 <adarsh.s@geekywolf.com>
2025-10-27 17:23:11 +0530

    day7 programming Assignment

---
+++ Day 7 Assignment/Programming Assignment/AverageExceptZero.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class ListExtensions
+{
+    public static double AverageExceptZero(this List<int> numbers)
+    {
+        if (numbers == null || numbers.Count == 0)
+            return 0;
+
+        var nonZeroNumbers = numbers.Where(n => n != 0);
+
+        if (!nonZeroNumbers.Any())
+            return 0;
+
+        return nonZeroNumbers.Average();
+    }
+}
+
