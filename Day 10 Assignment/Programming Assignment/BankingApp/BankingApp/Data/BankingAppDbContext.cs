879c5e18a728047a470385ffae1a76d5d4b4df0a
Adarsh-S-Kumar2025 <adarsh.s@geekywolf.com>
2025-11-04 14:00:42 +0530

    Day 10 C# assignment

---
+++ Day 10 Assignment/Programming Assignment/BankingApp/BankingApp/Data/BankingAppDbContext.cs
@@ -0,0 +1,37 @@
+﻿using BankingApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankingApp.Data
+{
+    public class BankingAppDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // ✅ Change your SQL Server connection string as per your setup
+            optionsBuilder.UseSqlServer("Server=localhost;Database=BankingAppDB;User Id=sa;Password=12345678Aa;TrustServerCertificate=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>()
+                .HasIndex(a => a.AccountNumber)
+                .IsUnique(); // AccountNumber must be unique
+
+            modelBuilder.Entity<Customer>()
+                .HasOne(c => c.Address)
+                .WithOne(a => a.Customer)
+                .HasForeignKey<Address>(a => a.CustomerId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Customer>()
+                .HasMany(c => c.Accounts)
+                .WithOne(a => a.Customer)
+                .HasForeignKey(a => a.CustomerId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
---
+++ Day 10 Assignment/Programming Assignment/BankingApp/BankingApp/Models/Account.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BankingApp.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(30)]
+        public string AccountNumber { get; set; }
+
+        public double Balance { get; set; }
+
+        [ForeignKey("Customer")]
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
